21ec2fb7f173fa96a09aece367ef1b7aa790a054
Tomasero <tomasvegagalvez@gmail.com>
2015-03-13 18:01:59 -0700

    forcing controller

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,21 @@
+class ItemsController < ApplicationController
+    def find
+
+    end
+    
+    def index
+        
+    end
+
+    def checkout
+       items = params[:items]
+       user_id = params[:user] 
+       total = 0.0
+       for item in items
+            # Call appropriate transaction function
+       end
+       flash[:notice] = "Successful transaction"
+       redirect items_path
+    end
+
+end
