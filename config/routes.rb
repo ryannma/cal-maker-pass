8deac13a10721b203632d6bfadce0c38e5b5dd61
Tomasero <tomasvegagalvez@gmail.com>
2015-04-03 23:21:25 -0700

    adding to cart madafaka

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Makerpass::Application.routes.draw do
   resources :items
-  get "/find" , to: "items#find", as: 'find'
-  post "/checkout" , to: "items#checkout", as: 'checkout'
-  
-  get "/logout", to: "application#logout", as: 'logout'
+  # resources :admins
+  # resources :transactions
+
+  get "/find" , to: "items#find", as: "find"
+  get "/logout", to: "application#logout", as: "logout"
+
+  #ITEM
+  post "/items/:id/update", to: "items#update", as: "update_item"
+  post "/items/:id/delete", to: "items#delete", as: "delete_item"
+  post "/items/checkout", to: "items#checkout", as: "checkout"
+  post "/items/add_item/:id", to: "items#add_item", as: "add_item"
 end
