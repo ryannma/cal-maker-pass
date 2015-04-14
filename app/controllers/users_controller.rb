f30e936eaa1d7b69885d42a1cf3caf47ede4950f
davyli <davy.li@berkeley.edu>
2015-04-14 11:46:33 -0700

    Added a signup views and fixed up all the user authentication logic & routing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
 class UsersController < ApplicationController
-  before_filter :check_user_exists, :except => [:new]
+  before_filter :check_user_exists, :except => [:new, :create]
 
   def new
+    @all_locations = Location.all
+  end
+
+  def create
+    user_params = params[:user]
+    user_params[:uid] = session[:cas_user]
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
+      flash[:notice] = "Your account has been created"
+    else
+      errors = @user.errors.full_messages.join("<br>").html_safe
+      puts errors
+      flash[:warning] = errors
+    end
+    redirect_to "/"
   end
 
 end
