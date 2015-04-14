a6ec72a4a16dbe06b2e7cc9d6d5acfd937e3ed13
davyli <davy.li@berkeley.edu>
2015-04-13 18:52:16 -0700

    Updating the routes and application controller methods for checking user authentication and whether or not the user is an admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :locations
   has_many :items, through: :transactions
   has_many :transactions
+
+  def admin?
+    admin = Admin.where(user_id: @id)
+    if admin.empty?
+      return false
+    else
+      return true
+    end
+  end
+
 end
