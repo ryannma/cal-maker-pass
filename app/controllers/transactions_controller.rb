ff1e887b26a651f98350dad842e50555c2b6cb58
Ryan Ma <ryanma21021@gmail.com>
2015-04-13 19:51:24 -0700

    Current work pre-redesign

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,57 +1,50 @@
 class TransactionsController < ApplicationController
 
   def index
-    puts params[:cart]
-    # user_id = params[:user_id]
-    # admin_id = params[:admin_id]
-    # Transaction.where(user_id: user_id, admin_id: admin_id).all
+    @transactions = Transaction.all
   end
 
-=begin
-   def index
-     user_id = params[:user_id]
-     admin_id = params[:admin_id]
-     Transaction.where(user_id: user_id, admin_id: admin_id).all
-   end
+  def create
+    @transaction = Transaction.new(params[:transaction])
+    if @transaction.valid?
+      @transaction.save
+    else
+      errors=@item.errors.full_messages.join("<br>").html_safe
+      puts errors
+      flash[:warning] = errors
+    end
+    redirect_to items_path
 
-   def show
-     trans_id = params[:trans_id]
-     params[:transaction] = Transaction.find_by_id(trans_id)
-   end
+  def deletee
+    @transaction = Transaction.find(params[:id])
+    @trasaction.destroy
+    redirect_to transactions_path
+  end
 
-   def new
-     params[:items] = [params[:item]]
-     redirect_to action: :new_bulk
-   end
+  def checkout
+    user_id = session[:cas_user]
+    user = User.find_by_uid(user_id)
+    admin_id = params[:admin_id]
+    admin = Admin.find(admin_id)
+    purpose = params[:purpose]
+    cart = params[:cart]
+    cart.each do |item|
+      item = Item.find(item[:id])
+      Transaction.create(user: user, admin: admin, purpose: purpose, kind: item.kind, item: item)
+    end
+    redirect_to items_path
+  end
 
-   def new_bulk
-     render :new_bulk
-   end
 
-   def create
-     user_id = params[:user_id]
-     admin_id= params[:admin_id]
-     items = params[:items]
-     trans_types = params[:types]
-     trans = Transaction.create!({item: item_id, user: user_id, admin: admin_id, mytype: trans_type})
-     params[:trans_id] = trans.id
-     flash[:notice] = "Successfully completed the transactions"
-     redirect_to action: :show
-   end
-=end
-
-   def create_bulk
-     trans_items = params[:items]
-     user_id = params[:user_id]
-     admin_id= params[:admin_id]
-     trans_ids = []
-     trans_items.each do |trans_item|
-       trans_ids << Transaction.create!({item: trans_item[:item], user: user_id, admin: admin_id, mytype: trans_item[:trans_type]}).id
-     end
-     redirect_to action: :index
-   end
-
-   def order
-   end
+  def create_bulk
+    trans_items = params[:items]
+    user_id = params[:user_id]
+    admin_id= params[:admin_id]
+    trans_ids = []
+    trans_items.each do |trans_item|
+      trans_ids << Transaction.create!({item: trans_item[:item], user: user_id, admin: admin_id, mytype: trans_item[:trans_type]}).id
+    end
+    redirect_to action: :index
+ end
 
 end
