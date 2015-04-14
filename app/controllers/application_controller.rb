6c6fa7a002faad78db7404323cea917608b58201
davyli <davy.li@berkeley.edu>
2015-04-13 18:52:16 -0700

    Updating the routes and application controller methods for checking user authentication and whether or not the user is an admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter CASClient::Frameworks::Rails::Filter
+  before_filter :check_user_exists
+
+  def home
+    if @user.admin?
+      redirect_to action "/items"
+    else
+      redirect_to action "/items"
+    end
+  end
 
   def logout
     CASClient::Frameworks::Rails::Filter.logout(self)
   end
+
+  private
+
+  def check_user_exists
+    @user = User.where(uid: session[:cas_user]).take
+    if @user.empty?
+      redirect_to action "/signup"
+    end
+  end
+
 end
