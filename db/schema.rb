65e20d8b224de231be7aa1c3446dec59cd85a401
Ryan Ma <ryanma21021@gmail.com>
2015-04-14 18:19:01 -0700

    Refactoring Transactions

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 
 ActiveRecord::Schema.define(:version => 20150415012314) do
 
+
   create_table "admins", :force => true do |t|
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
