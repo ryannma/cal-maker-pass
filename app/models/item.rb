7d56826aee087f2311c93ea829bc00817f4c2a1c
Ryan Ma <ryanma21021@gmail.com>
2015-04-14 18:28:11 -0700

    Checking in work

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ActiveRecord::Base
   attr_accessible :name, :quantity, :price, :kind, :status
   belongs_to :location
-  has_many :transactions
+  has_many :lineitems
   @@allowed_status = ["lend", "sell", "both"]
   validates :status, :inclusion=> { :in => @@allowed_status }
   validates :name, :price, :quantity, :kind, presence: true
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,7 @@
-class Transaction < ActiveRecord::Base
+class LineItem < ActiveRecord::Base
   attr_accessible :action, :quantity
   belongs_to :item
+  belongs_to :transaction
   allowed_types = ["sold", "lent", "returned"]
   validates :action, :inclusion=> { :in => allowed_types }
 end
