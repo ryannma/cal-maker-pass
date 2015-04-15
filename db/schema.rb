231f655622fa06621ae58f3ffe52c09061cc83f6
Ryan Ma <ryanma21021@gmail.com>
2015-04-14 18:19:01 -0700

    Refactoring Transactions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150414183341) do
+
+ActiveRecord::Schema.define(:version => 20150415004501) do
 
   create_table "admins", :force => true do |t|
     t.datetime "created_at",  :null => false
@@ -31,6 +32,14 @@ ActiveRecord::Schema.define(:version => 20150414183341) do
     t.integer  "location_id"
   end
 
+  create_table "line_items", :force => true do |t|
+    t.integer  "item_id"
+    t.integer  "quantity"
+    t.string   "action"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "locations", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
@@ -38,11 +47,9 @@ ActiveRecord::Schema.define(:version => 20150414183341) do
   end
 
   create_table "transactions", :force => true do |t|
-    t.string   "kind"
     t.string   "purpose"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.integer  "item_id"
     t.integer  "user_id"
     t.integer  "admin_id"
   end
