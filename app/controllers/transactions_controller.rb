93e984330c7eafab31466aceb35adeb3ed28d6b5
Ryan Ma <ryanma21021@gmail.com>
2015-04-16 02:06:37 -0700

    Checkout method

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -7,10 +7,24 @@ class TransactionsController < ApplicationController
 
   end
 
-  def add
-    admin = User.where(uid: session[:uid])
-    user = User.where(params[:student_id])
-    purpose = params[:purpose]
+  def checkout
+    admin_user = User.where(uid: session[:uid])
+    admin = Admin.where(user_id: admin_user.id)
+    cart = params[:cart]
+    user = User.where(id: cart.user.id)
+    tx = Transaction.new(:purpose=>cart.comment)
+    tx.user = user
+    tx.admin = admin
+    tx.save
+    items = cart.items
+    items.each do |cart_item|
+      lineitem = LineItem.new(:action => "sold", :quantity => cart_item.quantity)
+      inventory_item = Item.where(id: cart_item.item_id())
+      lineitem.item = inventory_item
+      lineitem.transaction = tx
+      lineitem.save
+    end
   end
+
 end
 
