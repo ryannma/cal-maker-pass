0a43444b511c2d274723d692c0d5bb8542926b60
rome-bop <park.srome@gmail.com>
2015-04-17 04:33:25 -0700

    refactored sorting/ordering

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,49 +1,7 @@
 class ItemsController < ApplicationController
     
   def index
-    if params.has_key?(:sort)
-      @sort = params[:sort]
-      session[:sort] = params[:sort]
-    elsif session.has_key?(:sort)
-      @sort = session[:sort]
-      need_redirect = true
-    else
-      @sort = nil
-    end
-
-    if params.has_key?(:sort_by) && session.has_key?(:sort_by) && (params[:sort_by] == session[:sort_by])
-      new_sort = false
-      @sort_type == 'ascending' ? (@sort_type = 'descending') : (@sort_type = 'ascending')
-    # accounts for case of having sorted one column to sorting a different column
-    elsif params.has_key?(:sort_by) && session.has_key?(:sort_by) && (params[:sort_by] != session[:sort_by])
-      new_sort = true
-      @sort_type = 'ascending'
-    elsif params.has_key?(:sort_by) && !session.has_key?(:sort_by)
-      new_sort = true
-      @sort_type = 'ascending'
-    else
-      new_sort = false
-      @sort_type = 'ascending'
-    end
-
-    if params.has_key?(:sort_by)
-      @sort_by = params[:sort_by]
-      should_sort = true
-      session[:sort_by] = params[:sort_by]
-    elsif session.has_key?(:sort_by)
-      @sort_by = session[:sort_by]
-      should_sort = false
-    else
-      @sort_by = nil
-      should_sort = false
-    end
-
-    if should_sort 
-      @items = Item.sort(@sort_by, @sort_type, new_sort)
-    else
-      @items = Item.all
-    end
-
+    @items = ordering()
     @all_status = Item.all_status
     session[:cart] = session[:cart] || Cart.new
     @cart = session[:cart]
@@ -135,19 +93,46 @@ class ItemsController < ApplicationController
   # end
 
   def query
-  # Get the search terms from the q parameter and do a search
-  # as we seen in the previous part of the article.
-  search = Item.search{ keywords params[:q]}
-  respond_to do |format|
-    format.json do
-      # Create an array from the search results.
-      results = search.results.map do |item|
-        # Each element will be a hash containing only the title of the article.
-        # The title key is used by typeahead.js.
-        { name: item.name }
+    # Get the search terms from the q parameter and do a search
+    # as we seen in the previous part of the article.
+    search = Item.search{ keywords params[:q]}
+    respond_to do |format|
+      format.json do
+        # Create an array from the search results.
+        results = search.results.map do |item|
+          # Each element will be a hash containing only the title of the article.
+          # The title key is used by typeahead.js.
+          { name: item.name }
+        end
+        render json: results
       end
-      render json: results
     end
   end
+
+  private
+
+  ## sorting logic
+  def ordering()
+    params.has_key?(:sort_by) ? (should_sort = true) : (should_sort = false)
+    session.has_key?(:sort_by) ? (sorted_before = true) : (sorted_before = false)
+    if should_sort
+      @sort_by = params[:sort_by]
+      # accounts for first time sorting in current session
+      if !sorted_before
+        @sort_type = 'ascending'
+      # accounts for clicking an already sorted column to change the sort type (ascending <=> descending)  
+      elsif sorted_before && (params[:sort_by] == session[:sort_by])
+        session[:sort_type] == 'ascending' ? (@sort_type = 'descending') : (@sort_type = 'ascending')
+      # accounts for having sorted one column and now sorting a different column
+      elsif sorted_before && (params[:sort_by] != session[:sort_by])
+        @sort_type = 'ascending'
+      end
+      session[:sort_by] = @sort_by
+      session[:sort_type] = @sort_type
+      Item.sort(@sort_by, @sort_type)
+    else
+      Item.all
+    end
   end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,14 +7,10 @@ class Item < ActiveRecord::Base
   validates :name, :price, :quantity, :kind, presence: true
   validates :price, :quantity, numericality: true
 
-  def self.sort(sort_by, sort_type, new_sort)
-    if new_sort
-      items = Item.order(sort_by)
-    else
-      sort_type == 'ascending' ? (items = Item.order(sort_by)) : (items = Item.order(sort_by).reverse_order)
-    end
+  def self.sort(sort_by, sort_type)
+    sort_type == 'ascending' ? (items = Item.order(sort_by)) : (items = Item.order(sort_by).reverse_order)
   end
-  
+
   def Item.all_status
     @@allowed_status
   end
