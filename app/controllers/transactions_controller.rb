88f94fc1deaa0a3328b0253b2043d2b9b4aa86fe
Ryan Ma <ryanma21021@gmail.com>
2015-04-16 19:38:35 -0700

    Mid-work on Transaction switch - Working on view all transactions while default is only transactions made by you

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -14,7 +14,7 @@ class TransactionsController < ApplicationController
       @sort = nil
     end
 
-    if (admin_user != nil and @all == false) 
+    if (admin_user != nil and params[:all] != false) 
       if @sort == 'customer'
         @transactions = Transaction.where(admin_id: admin_user.id).includes(:user).order("users.last_name")
       elsif @sort == 'purpose'
@@ -25,7 +25,7 @@ class TransactionsController < ApplicationController
         @transactions = Transaction.where(admin_id: admin_user.id)
       end
     else 
-      @transactions = []
+      @transactions = Transaction.all
     end
 
   end
