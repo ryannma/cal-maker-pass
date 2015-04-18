051683478922e877eecc8bdef4b249650df29d25
Ryan Ma <ryanma21021@gmail.com>
2015-04-17 17:46:18 -0700

    WIP

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -58,22 +58,22 @@ class TransactionsController < ApplicationController
   def checkout
     admin_user = User.where(uid: session[:cas_user])[0]
     admin = Admin.where(user_id: admin_user.id)[0]
-    cart = params[:hash]
-
-    # user = User.where(id: cart.user.id)
+    cart = session[:cart]
+    # user = User.where(id: cart['user'].id)
     tx = Transaction.new()
-    # tx.comment = cart.comment
+    tx.purpose = cart.comment
     # tx.user = user
     tx.admin = admin
     tx.save
     items = cart.cart_items
     items.each do |cart_item|
       lineitem = LineItem.new(:action => "sold", :quantity => cart_item.quantity)
-      inventory_item = Item.where(id: cart_item.item_id())
+      inventory_item = Item.where(id: cart_item.item_id())[0]
       lineitem.item = inventory_item
       lineitem.transaction = tx
       lineitem.save
     end
+    redirect_to items_path
   end
 
   def show
