67303c3ed59a96310f47c6cfab5f4c887320123f
rome-bop <park.srome@gmail.com>
2015-04-20 20:22:24 -0700

    fixing sorting code

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,8 @@
 class ItemsController < ApplicationController
     
   def index
-    sorts = choose_sort
-    @sort_by , @sort_type = sorts[0] , sorts[1]
-    set_session_sorting(@sort_by, @sort_type)
+    @sort_by, @sort_type = sort_params[0], sort_params[1]
+    set_session_sort(@sort_by, @sort_type)
     @items = Item.all
     @items = order_items(@sort_by, @sort_type, @items)
     @all_status = Item.all_status
@@ -73,25 +72,19 @@ class ItemsController < ApplicationController
   end
 
   def find
-    if params[:phrase].blank?
-      @items = Item.all
-    else
-      @items = Item.search(params[:phrase],fields: [{name: :word_start}], misspelling: {edit_distance: 2} , operator: "or").results
-    end
-    order_items(session[:sort_by],session[:sort_type], @items)
+    params[:phrase].blank? ? (@items = Item.all) : (@items = Item.search(params[:phrase],fields: [{name: :word_start}], misspelling: {edit_distance: 2} , operator: "or").results)
+    get_sort_params
+    Item.sort(@items, @sort_by, @sort_type)
     @all_status = Item.all_status
-    @sort_by = session[:sort_by]
     respond_to do |format|
       format.js{}
     end
   end
 
-    def query
+  def query
     # Get the search terms from the q parameter and do a search
     # as we seen in the previous part of the article.
     search = Item.search(params[:q],fields: [{name: :word_start}], misspelling: {edit_distance: 2} , operator: "or")
-    puts(search)
-    puts("fuuuuuuuuck")
     respond_to do |format|
       format.json do
         # Create an array from the search results.
@@ -106,44 +99,45 @@ class ItemsController < ApplicationController
   end
 
   private
-
-  def should_sort?(sort_by)
-    puts("should sort:#{sort_by} #{params}#{params.has_key?(sort_by)}")
+  ## sorting logic
+  def should_sort?
     params.has_key?(:sort_by)
   end
 
-  def sorted_before?(sort_by)
+  def sorted_before?
     session.has_key?(:sort_by)
   end
-  ## sorting logic
-  def choose_sort
-    sort_by = params[:sort_by]
-    puts(sort_by)
-    if should_sort?(sort_by)
+
+  def get_sort_params
+    if should_sort?
+      @sort_by = params[:sort_by]
       # accounts for first time sorting in current session
-      if !sorted_before?(sort_by)
-        sort_type = 'ascending'
-      # accounts for clicking an already sorted column to change the sort type (ascending <=> descending)  
-      elsif sorted_before?(sort_by) && (params[:sort_by] == session[:sort_by])
-        session[:sort_type] == 'ascending' ? (sort_type = 'descending') : (sort_type = 'ascending')
+      if !sorted_before?
+        @sort_type = 'ascending'
+      # accounts for clicking an already sorted column to change the sort type (ascending <-> descending)  
+      elsif sorted_before? && (params[:sort_by] == session[:sort_by])
+        session[:sort_type] == 'ascending' ? (@sort_type = 'descending') : (@sort_type = 'ascending')
       # accounts for having sorted one column and now sorting a different column
-      elsif sorted_before && (params[:sort_by] != session[:sort_by])
-        sort_type = 'ascending'
+      elsif sorted_before? && (params[:sort_by] != session[:sort_by])
+        @sort_type = 'ascending'
       end
-      puts("choose: #{sort_by}#{sort_type}")
-      return [sort_by , sort_type]
+    elsif sorted_before?
+      load_sort_params
+    else
+      @sort_by, @sort_type = nil, nil
     end
-    return [nil , nil]
   end
 
-  def order_items(sort_by, sort_type ,items)
-    puts(items)
-    Item.sort(sort_by, sort_type, items)
+  # remember most recent sort params
+  def save_sort_params
+    session[:sort_by] = @sort_by
+    session[:sort_type] = @sort_type
   end
 
-  def set_session_sorting(sort_by, sort_type)
-    session[:sort_by] = sort_by
-    session[:sort_type] = sort_type
-    puts('called')
+  # load sorting conditions from session
+  def load_sort_params
+    @sort_by = session[:sort_by]
+    @sort_type = session[:sort_type]
   end
+
 end
